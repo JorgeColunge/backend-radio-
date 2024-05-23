7fd52bcbb76513d201c16281b1e5f5ec0b47c292
JorgeColunge <cto.axiomarobotics@gmail.com>
2024-05-23 12:33:40 -0500

    Se modifico .env geolocationCOntroller y routes (server)

--- src/routes/geolocationRoutes.js
+++ src/routes/geolocationRoutes.js
@@ -3,20 +3,19 @@ const router = express.Router();
 const geolocationController = require('../controllers/geolocationController');
 
 module.exports = (io) => {
-  // Ruta para actualizar la ubicación de un usuario
   router.post('/update-location', (req, res) => geolocationController.updateUserLocation(req, res, io));
-
-  // Ruta para obtener la ubicación de un usuario específico
   router.get('/get-location/:id_usuario', geolocationController.getUserLocation);
-
-  // Ruta para obtener las ubicaciones de todos los usuarios
   router.get('/users', geolocationController.getAllUserLocations);
 
-  // Ruta para crear una nueva solicitud de taxi
   router.post('/taxi-request', (req, res) => geolocationController.requestTaxi(req, res, io));
-
-  // Ruta para aceptar una solicitud de taxi
   router.post('/accept-taxi-request', (req, res) => geolocationController.acceptTaxiRequest(req, res, io));
+  router.post('/retry-taxi-request', (req, res) => geolocationController.retryTaxiRequest(req, res, io));
+  router.post('/reject-taxi-request', (req, res) => geolocationController.rejectTaxiRequest(req, res, io));
+  router.get('/check-status/:viajeId', geolocationController.checkTaxiRequestStatus);
+
+  router.post('/delivery-request', (req, res) => geolocationController.requestDelivery(req, res, io));
+  router.post('/reservation-request', (req, res) => geolocationController.requestReservation(req, res, io));
 
   return router;
 };
+
--- src/server.js
+++ src/server.js
@@ -20,6 +20,16 @@ const io = socketIo(httpServer, {
 });
 
 io.on('connection', (socket) => {
+  console.log(`Nuevo cliente conectado: ${socket.id}`);
+
+  
+
+  socket.on('disconnect', () => {
+      console.log(`Cliente desconectado: ${socket.id}`);
+  });
+});
+
+io.on('connection', (socket) => {
   console.log(`Usuario conectado: ${socket.id}`);
 
   socket.on('registerUser', async (id_usuario) => {
